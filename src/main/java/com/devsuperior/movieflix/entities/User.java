5f8435f722f2ecfe33b668f95edffd10c6cad325
waner <wanersbh@gmail.com>
2021-10-19 13:56:40 -0300

    set RoleTests

--- src/main/java/com/devsuperior/movieflix/entities/User.java
+++ src/main/java/com/devsuperior/movieflix/entities/User.java
@@ -1,17 +1,29 @@
 package com.devsuperior.movieflix.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable {
+public class User implements UserDetails, Serializable {
 
 	private static final long serialVersionUID = -8400342134292262252L;
 	
@@ -21,6 +33,12 @@ public class User implements Serializable {
 	private String name;
 	private String email;
 	private String password;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_user_role",
+		joinColumns = @JoinColumn(name = "user_id"),
+		inverseJoinColumns = @JoinColumn(name = "role_id"))	
+	private Set<Role> roles = new HashSet<>();
 	
 	public User() {
 		super();
@@ -66,6 +84,21 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
+	public Set<Role> getRoles() {
+		return roles;
+	}
+	
+	public boolean hasHole(String roleName) {
+
+		for (Role role : roles) {
+			if (role.getAuthority().equalsIgnoreCase(roleName)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -81,6 +114,37 @@ public class User implements Serializable {
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		return getEmail();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 
 }
