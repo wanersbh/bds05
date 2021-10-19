29a6f575085efe74c4cfd14e902b431701b02291
waner <wanersbh@gmail.com>
2021-10-19 14:08:36 -0300

    set UserTests

--- src/main/java/com/devsuperior/movieflix/entities/User.java
+++ src/main/java/com/devsuperior/movieflix/entities/User.java
@@ -1,8 +1,10 @@
 package com.devsuperior.movieflix.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -15,6 +17,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.springframework.security.core.GrantedAuthority;
@@ -39,6 +42,9 @@ public class User implements UserDetails, Serializable {
 		joinColumns = @JoinColumn(name = "user_id"),
 		inverseJoinColumns = @JoinColumn(name = "role_id"))	
 	private Set<Role> roles = new HashSet<>();
+	
+	@OneToMany(mappedBy = "user")
+	private List<Review> reviews = new ArrayList<>();
 	
 	public User() {
 		super();
@@ -88,6 +94,10 @@ public class User implements UserDetails, Serializable {
 		return roles;
 	}
 	
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
 	public boolean hasHole(String roleName) {
 
 		for (Role role : roles) {
